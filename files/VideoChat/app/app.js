8c467fed2699d8ddca92be5d1feb2f148b85815a
aluaex@gmail.com <aluaex@gmail.com>
2015-06-29 22:24:08 +0200

    code video chat app

--- files/VideoChat/app/app.js
+++ files/VideoChat/app/app.js
@@ -1,2 +1,10 @@
 var app = angular.module('videoChat', []),
-    comm = new Icecomm('wxnPQomF6jnKHT1U4Tb4a5ISdWIYjNhESQLBAOtBxyUgkSCS');
+	comm = new Icecomm('wxnPQomF6jnKHT1U4Tb4a5ISdWIYjNhESQLBAOtBxyUgkSCS');
+
+app.config(function ($locationProvider) {
+	$locationProvider.html5Mode({
+		enabled: true,
+		requireBase: false,
+		rewriteLinks: false
+	});
+});
--- files/VideoChat/app/controllers/mainController.js
+++ files/VideoChat/app/controllers/mainController.js
@@ -1,6 +1,32 @@
-app.controller('ChatController', function ($scope) {
-    $scope.text = 'Some test string';
-    $scope.connect = function () {
-        comm.connect('publicRoom');
-    };
+app.controller('ChatController', function ($scope, $location, $sce) {
+	$scope.room = $location.hash();
+	$scope.local = null;
+	$scope.peers = [];
+	$scope.connect = function () {
+		comm.connect($scope.room);
+	};
+	$scope.leave = function () {
+		comm.leave();
+		$scope.local = null;
+	};
+
+	comm.on('local', function (peer) {
+		peer.stream = $sce.trustAsResourceUrl(peer.stream);
+		$scope.local = peer;
+
+		$scope.$apply();
+	});
+
+	comm.on('connected', function (peer) {
+		peer.stream = $sce.trustAsResourceUrl(peer.stream);
+		$scope.peers.push(peer);
+
+		$scope.$apply();
+	});
+
+	comm.on('disconnect', function (peer) {
+		$scope.peers.splice($scope.peers.indexOf(peer), 1);
+
+		$scope.$apply();
+	});
 });
