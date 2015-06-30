60f061b3a82ce631b5e6da6044c71efac11735aa
aluaex@gmail.com <aluaex@gmail.com>
2015-07-01 00:00:48 +0200

    ng-repeat video tags

--- files/VideoChat/app/controllers/mainController.js
+++ files/VideoChat/app/controllers/mainController.js
@@ -1,81 +1,79 @@
-app.controller('ChatController', function ($scope, $location, $sce, $log, $timeout) {
-    $scope.room = $location.hash();
-    $scope.local = null;
-    $scope.peers = [];
-    $scope.peerCont = $('.peers-container');
-    $scope.getVideo = function(data){
-        return '<video autoplay src="'+ data.trustStream +'" id="'+ data.ID +'"></video>';
-    };
-    $scope.appendVideo = function($cont, data){
-        $cont.append($scope.getVideo(data));
-    };
-    $scope.removeVideo = function(ID){
-        $('#'+ ID).remove();
-    };
-    $scope.connect = function () {
-        comm.connect($scope.room, {
-            audio: false
-        });
-    };
-    $scope.leave = function () {
-        comm.leave();
-        $scope.peers = [];
-        $scope.local = null;
-    };
-    $scope.getTrustStream = function (peerArray){
-        angular.forEach(peerArray, function(val, key) {
-            val.trustStream = $sce.trustAsResourceUrl(val.stream);
-        });
-        
-        return peerArray;
-    };
-    $scope.sendData = function (data, type, ID) {
-        var sendData = angular.extend({}, {
-            data: data,
-            type: type,
-            forID: ID
-        });
-        
-        comm.send(sendData);
-    };
-    $scope.routeData = function (res) {
-        var dataType = res.data.type,
-            forID = res.data.forID,
-            data = res.data.data;
-            
-        if(forID != undefined && forID != $scope.local.ID)
-            return;
+app.controller('ChatController', function ($scope, $location, $sce, $log, $timeout, $compile) {
+	$scope.room = $location.hash();
+	$scope.local = null;
+	$scope.peers = {};
+	$scope.peerCont = angular.element(document.querySelector('.peers-container'));
+	$scope.connect = function () {
+		comm.connect($scope.room, {
+			audio: false
+		});
+	};
+	$scope.leave = function () {
+		comm.leave();
+		$scope.peers = {};
+		$scope.local = null;
+	};
+	$scope.savePeer = function(peer){
+		var peerToSave = {};
+		peerToSave[peer.ID] = peer;
+		angular.extend($scope.peers, peerToSave);
+	};
+	$scope.getTrustStream = function (peer) {
+		if(peer.stream)
+			peer.trustStream = $sce.trustAsResourceUrl(peer.stream);
+		else{
+			angular.forEach(peer, function (val, key) {
+				val.trustStream = $sce.trustAsResourceUrl(val.stream);
+			});
+		}
 
-        $log.debug(data);
+		return peer;
+	};
+	$scope.sendData = function (data, type, ID) {
+		var sendData = angular.extend({}, {
+			data: data,
+			type: type,
+			forID: ID
+		});
 
-        switch (dataType) {
-            case 'connected':
-                $scope.peerConnected(data);
-                break;
-            case 'setPeers':
-                $scope.setPeers(data);
-                break;
-            default:
-                $log.debug('Income data has no route.');
-                break;
-        }
-    };
-    $scope.addPeer = function(peer){
-        
-    };
-    comm.on('data', $scope.routeData);
-    comm.on('local', function (peer) {
-        $scope.local = $scope.getTrustStream([peer])[0];
+		comm.send(sendData);
+	};
+	$scope.routeData = function (res) {
+		var dataType = res.data.type,
+			forID = res.data.forID,
+			data = res.data.data;
 
-        $scope.$apply();
-    });
-    comm.on('connected', function(peer){
-        $scope.peers.push($scope.getTrustStream([peer])[0]);
-        $scope.appendVideo($scope.peerCont, peer);
-    });
-    comm.on('disconnect', function (peer) {
-        $scope.peers.splice($scope.peers.indexOf(peer), 1);
-        
-        $scope.removeVideo(peer.ID);
-    });
+		if (forID != undefined && forID != $scope.local.ID)
+			return;
+
+		$log.debug(data);
+
+		switch (dataType) {
+		case 'connected':
+			$scope.peerConnected(data);
+			break;
+		case 'setPeers':
+			$scope.setPeers(data);
+			break;
+		default:
+			$log.debug('Income data has no route.');
+			break;
+		}
+	};
+	comm.on('data', $scope.routeData);
+	comm.on('local', function (peer) {
+		$timeout(function () {
+			$scope.local = $scope.getTrustStream([peer])[0];
+		});
+	});
+	comm.on('connected', function (peer) {
+		$timeout(function () {
+			$scope.savePeer($scope.getTrustStream(peer));
+		});
+	});
+	comm.on('disconnect', function (peer) {
+		$timeout(function () {
+			delete $scope.peers[peer.ID];
+		});
+	});
 });
