9f1ad114cad76d8565446344f0184f3b9447c604
alexxxua <aluaex@gmail.com>
2015-06-30 16:03:03 +0200

    revert to the connected event

--- files/VideoChat/app/app.js
+++ files/VideoChat/app/app.js
@@ -1,5 +1,5 @@
 var app = angular.module('videoChat', []),
-	comm = new Icecomm('wxnPQomF6jnKHT1U4Tb4a5ISdWIYjNhESQLBAOtBxyUgkSCS');
+    comm = new Icecomm('wxnPQomF6jnKHT1U4Tb4a5ISdWIYjNhESQLBAOtBxyUgkSCS');
 
 app.config(function ($locationProvider) {
 	$locationProvider.html5Mode({
