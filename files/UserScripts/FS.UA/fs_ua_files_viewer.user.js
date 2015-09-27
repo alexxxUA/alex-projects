cf011b10c3387e331e3388533f249ae68f42a599
alexxxUA <aluaex@gmail.com>
2015-09-27 13:31:26 +0200

    Change version

--- files/UserScripts/FS.UA/fs_ua_files_viewer.user.js
+++ files/UserScripts/FS.UA/fs_ua_files_viewer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         FS.UA files viewer
-// @version      4.1
+// @version      5
 // @description  FS.UA files (video, audio, games, etc...) viewer from non UA/RU coutries
 // @author       Alexey
 // @match        http://brb.to/*
