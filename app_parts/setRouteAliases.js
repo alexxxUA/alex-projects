520375a8a82b65d56063ca013f7c6a5b953e7d42
alexxxUA <aluaex@gmail.com>
2015-11-11 00:10:39 +0100

    Partially finished UI part

--- app_parts/setRouteAliases.js
+++ app_parts/setRouteAliases.js
@@ -5,15 +5,13 @@ function init(app, callback){
     Aliases.find({}, function(err, aliases){
         if(err) throw err;
 
-        aliases.forEach(function(alias, index){
+        aliases.forEach(function(alias){
             app.get('/'+ alias.alias, function(req, res){
                 res.redirect('/'+ alias.path); 
             });
-
-            //Fire callback
-            if(index + 1 == aliases.length)
-                callback();
         });
+        //Fire callback
+        callback();
     });
 }
 
