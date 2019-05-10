a1248889016190ec3230ea4ecd821d1d53bc45a8
alexxxUA <aluaex@gmail.com>
2019-05-10 22:35:26 +0200

    Update icons. Fixed sk names for program

--- config/channelList_sk.js
+++ config/channelList_sk.js
@@ -1,14 +1,14 @@
 var channelListSk = [
 	{dName: 'Kosice dnes', sName: '', flags: 'm3'},
 	{dName: 'Markíza', sName: 'Markíza|TV Markíza|Markiza|TV Markiza', flags: 'm3'},
-	{dName: 'JOJ FAMILY', sName: '', flags: 'm3'},
-	{dName: 'JOJ HD', sName: 'TV JOJ 720p', flags: 'm3'},
-	{dName: 'JOJ Plus HD', sName: 'JOJ Plus', flags: 'm3'},
+	{dName: 'JOJ Family', sName: '', flags: 'm3'},
+	{dName: 'JOJ HD', sName: 'TV JOJ 720p', pName: 'JOJ', flags: 'm3'},
+	{dName: 'JOJ Plus HD', sName: 'JOJ Plus', pName: 'JOJ Plus', flags: 'm3'},
 	{dName: 'Dajto', sName: 'Dajto|TV Dajto|Dajto HD', flags: 'm3'},
-	{dName: 'Doma HD', sName: 'Doma', flags: 'm3'},
-	{dName: 'WAU HD', sName: 'WAU', flags: 'm3'},
-	{dName: 'Jednotka', sName: 'Jednotka|STV 1', flags: 'm3'},
-	{dName: 'Dvojka', sName: 'Dvojka|STV 2', flags: 'm3'},
+	{dName: 'Doma HD', sName: 'Doma', pName: 'Doma', flags: 'm3'},
+	{dName: 'WAU HD', sName: 'WAU', pName: 'WAU', flags: 'm3'},
+	{dName: 'Jednotka', sName: 'Jednotka|STV 1', pName: 'STV1', flags: 'm3'},
+	{dName: 'Dvojka', sName: 'Dvojka|STV 2', pName: 'STV2', flags: 'm3'},
 	{dName: 'CE TV', sName: '', flags: 'm3'},
 	{dName: 'TA3', sName: '', flags: 'm3'}
 ]
