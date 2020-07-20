50940dd9794063eb3e9229e8c44fe6c82a6bf398
a.vasin <a.vasin@astoundcommerce.com>
2020-07-20 18:40:32 +0200

    add lang

--- config/channelList_sk.js
+++ config/channelList_sk.js
@@ -1,17 +1,17 @@
 var channelListSk = [
-	{dName: 'Jojko', sName: '', tvgId: 'RiK', flags: 'm3'},
-	{dName: 'Kosice dnes', sName: '', flags: 'm3'},
-	{dName: 'Markíza', sName: 'Markíza|TV Markíza|Markiza|TV Markiza', flags: 'm3'},
-	{dName: 'JOJ Family', sName: '', tvgId: 'JOJ Family', flags: 'm3'},
-	{dName: 'JOJ HD', sName: 'TV JOJ 720p', tvgId: 'JOJ.sk', flags: 'm3'},
-	{dName: 'JOJ Plus HD', sName: 'JOJ Plus', tvgId: 'JOJ Plus', flags: 'm3'},
-	{dName: 'Dajto', sName: 'Dajto|TV Dajto|Dajto HD', tvgId: 'Dajto', flags: 'm3'},
-	{dName: 'Doma HD', sName: 'Doma', tvgId: 'Doma', flags: 'm3'},
-	{dName: 'WAU HD', sName: 'WAU', tvgId: 'WAU', flags: 'm3'},
-	{dName: 'Jednotka', sName: 'Jednotka|STV 1', tvgId: 'STV1', flags: 'm3'},
-	{dName: 'Dvojka', sName: 'Dvojka|STV 2', tvgId: 'STV2', flags: 'm3'},
-	{dName: 'CE TV', sName: '', flags: 'm3'},
-	{dName: 'TA3', sName: '', flags: 'm3'}
+	{dName: 'Jojko', sName: '', tvgId: 'RiK', flags: 'm3', lang: 'cs'},
+	{dName: 'Kosice dnes', sName: '', flags: 'm3', lang: 'cs'},
+	{dName: 'Markíza', sName: 'Markíza|TV Markíza|Markiza|TV Markiza', flags: 'm3', lang: 'cs'},
+	{dName: 'JOJ Family', sName: '', tvgId: 'JOJ Family', flags: 'm3', lang: 'cs'},
+	{dName: 'JOJ HD', sName: 'TV JOJ 720p', tvgId: 'JOJ.sk', flags: 'm3', lang: 'cs'},
+	{dName: 'JOJ Plus HD', sName: 'JOJ Plus', tvgId: 'JOJ Plus', flags: 'm3', lang: 'cs'},
+	{dName: 'Dajto', sName: 'Dajto|TV Dajto|Dajto HD', tvgId: 'Dajto', flags: 'm3', lang: 'cs'},
+	{dName: 'Doma HD', sName: 'Doma', tvgId: 'Doma', flags: 'm3', lang: 'cs'},
+	{dName: 'WAU HD', sName: 'WAU', tvgId: 'WAU', flags: 'm3', lang: 'cs'},
+	{dName: 'Jednotka', sName: 'Jednotka|STV 1', tvgId: 'STV1', flags: 'm3', lang: 'cs'},
+	{dName: 'Dvojka', sName: 'Dvojka|STV 2', tvgId: 'STV2', flags: 'm3', lang: 'cs'},
+	{dName: 'CE TV', sName: '', flags: 'm3', lang: 'cs'},
+	{dName: 'TA3', sName: '', flags: 'm3', lang: 'cs'}
 ]
 
 module.exports.channelListSk = channelListSk;
