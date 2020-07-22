a7872695dd308b36cb7f007861c572815c9fc173
a.vasin <a.vasin@astoundcommerce.com>
2020-07-22 17:06:58 +0200

    Add xz -> gz

--- app_parts/extract.js
+++ app_parts/extract.js
@@ -1,24 +1,26 @@
 const lzma = require('lzma-native');
+const zlib = require('zlib');
 const needle = require('needle');
 
 const Extract = {
-    async xz(req, res){
+    async xz(req, res) {
         const file = await this.downloadFile(req);
 
         if(file.err) {
             res.status(404).send(file.err);
         } else {
-            const fileName = this.getFileName(req.query.url).replace('.xz', '.xml');
             const extractedFile = await this.extract(file);
+            const gzipFile = await this.gzipFile(extractedFile);
+            const fileName = this.getFileName(req.query.url).replace('.xz', '.xml.gz');
 
             res.header({
                 'Content-Type': 'text/xml',
                 'Content-Disposition': `attachment; filename="${fileName}"`,
             });
-            res.status(200).send(extractedFile);
+            res.status(200).send(gzipFile);
         }
     },
-    downloadFile(req){
+    downloadFile(req) {
         const url = req.query.url;
 
         if(!url) {
@@ -52,7 +54,15 @@ const Extract = {
             })
     },
 
-    getFileName(url){
+    gzipFile(file) {
+        return new Promise((resolve) =>
+            zlib.gzip(file, (err, result) => {
+                resolve(result);
+            })
+        );
+    },
+
+    getFileName(url) {
         return url.split('/').pop();
     }
 }
