cab098b8ecca0f63e42f8c8ba22e86d387f663b4
a.vasin <a.vasin@astoundcommerce.com>
2020-10-25 20:32:12 +0100

    Test email auth

--- app_parts/sendMail.js
+++ app_parts/sendMail.js
@@ -4,7 +4,7 @@ const { SMTPClient } = require('emailjs');
 var server = new SMTPClient({
 	user: cf.email,
 	password: cf.emailPass,
-	host: "smtp.gmail.com",
+	host: `smtp.${cf.email}`,
 	ssl: true
 });
 
